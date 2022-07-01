7c0fe6d33884a8e9958adab40dcbd27dbe4002bf
luluulemon <chen_luwei@yahoo.com.sg>
2022-07-01 10:55:01 +0800

    program+comments

--- src/main/java/vtp2022/assessment/Main.java
+++ src/main/java/vtp2022/assessment/Main.java
@@ -7,9 +7,20 @@ package vtp2022.assessment;
 public class Main 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        String data = "tour_packages.csv";
+        String template = "tour_packages.txt";
+
         if (args.length>0)
-        {   }
+        {   data = args[0];
+            template = args[1]; 
+            readfile.read(data, template);
+        }
+        else 
+        {    System.out.println("Please specify file sources"); }
+
+        
+
+
     }
 }
---
+++ src/main/java/vtp2022/assessment/readfile.java
@@ -0,0 +1,67 @@
+package vtp2022.assessment;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class readfile {
+    
+    
+    public static void read(String data, String template){
+        try{
+        InputStream is = new FileInputStream(data);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String[] headers = br.readLine().split(",");    // get headers for data
+
+        String line;
+        List<String> templateData = new LinkedList<>();
+        while (null!=(line = br.readLine()))
+        {   templateData.add(line);    }     // add non-headers data to list                   
+          
+        System.out.println(templateData.size());    // print check
+
+        br.close();
+        isr.close();
+        is.close();
+ 
+        // add underscore to headers to replace template
+        String[] modifiedHeaders = new String[headers.length];
+        for(int i=0;i<headers.length;i++)
+        {   modifiedHeaders[i] = "__" + headers[i] + "__" ; }
+
+
+        System.out.println(Arrays.toString(modifiedHeaders));   // print Check
+
+            // loop each datapoint through template
+            for(String eachData:templateData)
+            {   
+                System.out.println("\n");
+                InputStream is2 = new FileInputStream(template);
+                InputStreamReader isr2 = new InputStreamReader(is2);
+                BufferedReader br2 = new BufferedReader(isr2);
+                while(null!=(line=br2.readLine()))
+                {   
+                    for(int i=0;i<headers.length; i++)
+                    {   if (line.contains(modifiedHeaders[i])   )
+                        {   String target = modifiedHeaders[i];
+                            String replacement = eachData.split(",")[i];
+                            line = line.replace(target, replacement);                  
+                        }                  
+                    }
+                    System.out.println(line);               
+                }
+                br2.close();
+            }
+
+        }
+        catch(IOException e)
+        {   System.err.println(e);  }
+    }
+}
