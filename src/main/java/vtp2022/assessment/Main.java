cce802926cb69c1f9901a3117fcc9cde308c1457
luluulemon <chen_luwei@yahoo.com.sg>
2022-07-01 09:18:40 +0800

    first commit

---
+++ src/main/java/vtp2022/assessment/Main.java
@@ -0,0 +1,15 @@
+package vtp2022.assessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        if (args.length>0)
+        {   }
+    }
+}
